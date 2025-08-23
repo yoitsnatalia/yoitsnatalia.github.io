a576b3b96a6e577ae85b2304e833bdb46dff1879
Natalia Linn <natalial@stanford.edu>
2025-08-22 22:45:29 -0600

    mobile clubhub page

--- client/src/pages/ClubHubPage.jsx
+++ client/src/pages/ClubHubPage.jsx
@@ -6,8 +6,8 @@ export default function ClubHubPage({setPage}) {
                 <h1>ClubHub</h1>
                 <button className="back" onClick={() => setPage('home')}>back</button>
             </div>
-            <h3 className="px-1 pb-1 text-3xl font-bold self-center">TreeHacks 2024</h3>
-            <h3 className="px-1 pb-5 text-xl self-center">Team: Natalia Linn, Bryce Kammerzell, Ben Friedman, & Daniel Yang</h3>
+            <h3 className="px-1 pb-1 text-3xl font-bold md:self-center">TreeHacks 2024</h3>
+            <h3 className="px-1 pb-5 pt-3 md:text-xl self-center">Team: Natalia Linn, Bryce Kammerzell, Ben Friedman, & Daniel Yang</h3>
             <h3 className="px-1 pb-5 text-xl italic self-center">A one-stop shop for Stanford students to easily discover and join campus communities and events anytime, all year long.</h3>
             <ClubHubSlides/>
             <div className="flex justify-center md:mt-5">
